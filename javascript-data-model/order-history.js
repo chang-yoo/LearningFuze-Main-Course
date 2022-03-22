cf8f7bbc86f51c511372d3f86847b345778da104
Changhyun Yoo <chyoo0428@gmail.com>
2022-03-22 16:11:46 -0700

    Revised javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -6,13 +6,13 @@ var orderHistory = [
     orderNumber: '114-3941689-8772232',
     delivered: 'Aug 8, 2020',
     handToResident: false,
-    item: {
+    item: [
       title: 'JavaScript for impatient programmers',
       author: 'Rauschmayer, Dr. Axel',
       returnDate: 'Sep 7, 2020',
       price: 31.55,
       itemImage: 'images/url.png',
-    }
+    ]
   },
   {
     orderedPlaced: 'July 19, 2020',
@@ -21,13 +21,13 @@ var orderHistory = [
     orderNumber: '113-9984268-1280257',
     delivered: 'July 20, 2020',
     handToResident: true,
-    item: {
+    item: [
       title: 'The Timeless Way of Building',
       author: 'Alexander, Chirstopher',
       returnDate: 'Aug 19, 2020',
       price: 41.33,
       itemImage: 'images/url2.png',
-    }
+    ]
   },
   {
     orderedPlaced: 'July 4, 2020',
@@ -36,13 +36,13 @@ var orderHistory = [
     orderNumber: '114-2875557-9059409',
     delivered: 'July 7, 2020',
     handToResident: true,
-    item: {
+    item: [
       title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
       author: null,
       returnDate: 'Aug 5, 2020',
       price: 15.98,
       itemImage: 'images/url3.png',
-    }
+    ]
   },
   {
     orderedPlaced: 'July 3, 2020,',

b6634c4a795e243f366b8355e6aa029ddadc6d3e
Changhyun Yoo <chyoo0428@gmail.com>
2022-03-23 21:07:27 -0700

    Final revised css-canvas-medical

---
+++ javascript-loops/main.js
@@ -0,0 +1,19 @@
+/* exported getNumbersToTen,
+            getEvenNumbersToTwenty,
+            repeatWord,
+            logEachCharacter,
+            doubleAll,
+            getKeys,
+            getValues
+ */
+
+function getNumbersToTen(){
+  var numbers = [];
+  var currentNumber = 1;
+  while (currentNumber<=10){
+    numbers.push(currentNumber);
+    currentNumber++;
+  }
+  return numbers;
+}
+console.log(getNumbersToTen())

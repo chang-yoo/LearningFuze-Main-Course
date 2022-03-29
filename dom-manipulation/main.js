0fa5153206ab38d2b042a6d6ebef631ec9a1cbbe
Changhyun Yoo <chyoo0428@gmail.com>
2022-03-28 20:31:38 -0700

    Completed dom-manipulation

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -1,14 +1,26 @@
-var number = 0;
+var click = 0;
 
 var $hotButton = document.querySelector('.hot-button');
 var $clickCount = document.querySelector('.click-count');
 
-console.log(number);
 
-$hotButton.addEventListener('click', increment)
-
-function increment(event){
-   number++;
+function temperature() {
+  if (click < 4) {
+    return 'hot-button.cold'
+  } else if (click < 7) {
+    return 'hot-button.cool'
+  } else if (click < 10) {
+    return 'hot-button.tepid'
+  } else if (click < 13) {
+    return 'hot-button.warm'
+  } else if (click < 16) {
+    return 'hot-button.hot'
+  } else {
+    return 'hot-button.nuclear'
+  }
 };
 
-$clickCount.textContent = 'Clicks: ' + number
+$hotButton.addEventListener('click', function(){
+  click++;
+  $clickCount.textContent = 'Clicks: ' + click;
+});

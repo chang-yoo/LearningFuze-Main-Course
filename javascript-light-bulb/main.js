90d7acbdbdc490c368e53a211178f30043c9d3c4
Changhyun Yoo <chyoo0428@gmail.com>
2022-03-29 21:10:38 -0700

    Revised javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,22 +1,15 @@
 $button = document.querySelector('.circle');
 $background = document.querySelector('.background');
-var click = ''
+var click = true;
 
 $button.addEventListener('click', function(){
-  $button.className = 'circle ' + black()
-  $background.className = 'background ' + blackcircle()
-  click++;
-})
-
-function black(){
-  if (click%2 !== 0) {
-    return 'black'
+  if (click === true){
+    $button.className = 'circle black';
+    $background.className = 'background black-color';
+    click = false;
+  } else {
+    $button.className = 'circle yellow';;
+    $background.className = 'background yellow-color';
+    click = true;
   }
-}
-
-
-function blackcircle(){
-  if (click%2 !== 0){
-  return 'black-circle'
-  }
-}
+});

d3d1239353df6510662006124b2a61cc9bdc3bb1
Changhyun Yoo <chyoo0428@gmail.com>
2022-03-30 22:21:53 -0700

    Completed  javascript-modal

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,17 +1,16 @@
-$container = document.querySelector('.container');
+$layout = document.querySelector('.layout');
 $modal = document.querySelector('.modal');
 $fixed = document.querySelector('.fixed');
-$buttonBox = document.querySelector('.button-box');
 $popUpButton = document.querySelector('.pop-up-button');
 
 $modal.addEventListener('click', function(){
   $fixed.classList.remove('none');
-  $container.className = 'container ' + 'background-color';
+  $layout.className = 'layout ' + 'background-color';
   $modal.className = 'modal ' + 'pointer-none';
 });
 
 $popUpButton.addEventListener('click', function(){
   $fixed.className = 'fixed ' + 'none';
-  $container.classList.remove('background-color');
-  $modal.classList.remove('pointer-none');
+  $layout.className ='layout';
+  $modal.className = 'modal';
 })

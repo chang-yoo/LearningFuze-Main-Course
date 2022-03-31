6c235fd4a77e393204514059019900915721d3fe
Changhyun Yoo <chyoo0428@gmail.com>
2022-03-31 14:54:51 -0700

    Revised javascript-view-swapping

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -5,15 +5,22 @@ console.log($tab);
 var $view = document.querySelectorAll('.view');
 console.log($view);
 
-
 $tabContainer.addEventListener('click', function matches(event){
     for (var i = 0; i < $tab.length; i++ ){
       if ($tab[i] === event.target){
         $tab[i].className = 'tab active';
-        $view[i].className = 'view'
       } else {
         $tab[i].className = 'tab';
-        $view[i].className = 'view hidden';
       }
     };
-  });
+
+    var dataView = event.target.getAttribute('data-view');
+    for (var q = 0; q < $view.length; q++){
+    var $dataView = $view[q].getAttribute('data-view');
+      if (dataView === $dataView){
+      $view[q].className = 'view';
+    } else {
+      $view[q].className = 'view hidden';
+    }
+  }
+});

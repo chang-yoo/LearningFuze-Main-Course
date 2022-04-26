f33feec9e292a8b90c19ca556fa3a06c3d0f6d52
Changhyun Yoo <chyoo0428@gmail.com>
2022-04-26 22:10:54 +0000

    in progress

---
+++ string-manipulation-3/src/is-anagram.js
@@ -0,0 +1,23 @@
+/* exported isAnagram */
+/*
+create a function definition that compares each letter of firstString to secondString
+convert each argument to array and use sort() method to set up the letters in alphabetical order and convert back to string
+compare two variables if they are same
+if so, return true and if not return false
+*/
+
+function isAnagram(firstString, secondString) {
+  var noSpace1 = firstString.replaceAll(' ', '');
+  var noSpace2 = secondString.replaceAll(' ', '');
+  var arr1 = noSpace1.split('');
+  var arr2 = noSpace2.split('');
+  var sortArr1 = arr1.sort();
+  var sortArr2 = arr2.sort();
+  var string1 = sortArr1.join('');
+  var string2 = sortArr2.join('');
+  if (string1 === string2) {
+    return true;
+  } else {
+    return false;
+  }
+}
---
+++ string-manipulation-3/src/reverse-words.js
@@ -0,0 +1,26 @@
+/* exported reverseWords */
+/* exported reverseWords */
+/*
+create a function definition that returns each word reversed, but the word in their original order
+convert in to array using split(' ') so it separates to each index when there is a space
+for loop to put the value backwards for each index
+convert back to string
+*/
+
+function reverseWords(string) {
+  var empty = [];
+  var toArr = string.split(' ');
+
+  for (var i = 0; i < toArr.length; i++) {
+    for (var q = toArr[i].length; q >= 0; q--) {
+      if (toArr[i][q] !== undefined) {
+        empty.push(toArr[i][q]);
+      } else {
+        empty.push(' ');
+      }
+    }
+  }
+  var outcome = empty.join('');
+  var result = outcome.substr(1);
+  return result;
+}

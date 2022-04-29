8e5c29e129e331dbf9ffd44733bef8c798a3ff7e
Changhyun Yoo <chyoo0428@gmail.com>
2022-04-29 18:39:17 +0000

    feature zip completed

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,32 @@
 /* exported zip */
+/*
+function definition with two parameters that would return values in same index number in each array
+asssign an empty array to a variable1
+assign another empty arry to a variable2
+create if statement to set each parameter's equal to each other
+create a loop and set the conditions to length of first parameter
+create a if statement to set if they have same index number
+push it to empty array variable
+create another for loop to slice two values in variabl 1 so the new value has each varaible 1 with array
+return the variable2
+*/
+
+function zip(first, second) {
+  var arr = [];
+  var arr2 = [];
+  if (first.length > second.length) {
+    first.length = second.length;
+  }
+  for (var i = 0; i < first.length; i++) {
+    var firstValue = first[i];
+    var secondValue = second[i];
+    arr.push(firstValue, secondValue);
+  }
+  for (var q = 0; q < arr.length; q++) {
+    if (q % 2 === 0) {
+      var sliced = arr.slice(q, q + 2);
+      arr2.push(sliced);
+    }
+  }
+  return arr2;
+}

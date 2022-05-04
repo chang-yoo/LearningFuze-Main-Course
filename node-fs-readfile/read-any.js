210300793f3c2458b819f9ab10f4dbc51e48f7aa
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-04 21:43:22 +0000

    second feature completed

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -1,3 +1,19 @@
-const fs = require('fs');
 
-fs.readFile();
+const fs = require('fs');
+const [, , files] = process.argv;
+
+fs.readFile(files, 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  } else if (files === 'hopper.txt') {
+    console.log(data);
+  } else if (files === 'hipster-ipsum.txt') {
+    console.log(data);
+  } else if (files === 'dijkstra.txt') {
+    console.log(data);
+  } else if (files === 'cunningham.txt') {
+    console.log(data);
+  } else if (files === 'read-dijkstra.js') {
+    console.log(data);
+  }
+});

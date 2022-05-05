135961fa19ffe978d02d88b507c7f223c819f8d9
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-05 18:27:09 +0000

    featured 3 completed

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -9,9 +9,22 @@ if (keyword === 'read') {
     console.log(`${numb}: ${notes[numb]}`);
   }
 }
+
 if (keyword === 'create') {
   json.notes[nextId] = process.argv[3];
   json.nextId++;
+  const stringjson = JSON.stringify(json, null, 2);
+  fs.writeFile('data.json', stringjson, 'utf8', err => {
+    if (err) {
+      console.err(err);
+      process.exit(1);
+    }
+  });
+}
+
+if (keyword === 'delete') {
+  const id = process.argv[3];
+  delete notes[id];
   const stringjson = JSON.stringify(json, null, 2);
   fs.writeFile('data.json', stringjson, 'utf8', err => {
     if (err) {

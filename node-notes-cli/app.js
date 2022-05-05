e06752c7d6640e278a9abe0f823788061c014721
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-05 18:30:35 +0000

    all featured completed

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -25,6 +25,20 @@ if (keyword === 'create') {
 if (keyword === 'delete') {
   const id = process.argv[3];
   delete notes[id];
+  json.nextId--;
+  const stringjson = JSON.stringify(json, null, 2);
+  fs.writeFile('data.json', stringjson, 'utf8', err => {
+    if (err) {
+      console.err(err);
+      process.exit(1);
+    }
+  });
+}
+
+if (keyword === 'update') {
+  const id = process.argv[3];
+  const updated = process.argv[4];
+  notes[id] = updated;
   const stringjson = JSON.stringify(json, null, 2);
   fs.writeFile('data.json', stringjson, 'utf8', err => {
     if (err) {

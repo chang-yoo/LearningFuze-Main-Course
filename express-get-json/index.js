f0c24b5e219227d9c5f910737ef091676d0b7406
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-06 22:15:09 +0000

    completed express-get-json

---
+++ express-get-json/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+const app = express();
+
+app.get('/api/grades', (req, res) => {
+  const arr = [];
+  for (const prop in grades) {
+    arr.push(grades[prop]);
+  }
+  res.send(arr);
+});
+
+app.listen(3000, () => {
+  // console.log('listening on port 3000');
+});

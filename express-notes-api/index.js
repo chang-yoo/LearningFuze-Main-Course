6d65484d974ca78478a92f8c7b0f9c7fb89935be
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-09 23:46:03 +0000

    completed 2nd feature

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -13,6 +13,18 @@ app.get('/api/notes', (req, res) => {
   res.status(200).json(arr);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const beforeId = req.params.id;
+  const id = parseInt(beforeId);
+  if (id < 0 || isNaN(id) || !Number.isInteger(id)) {
+    res.status(400).send('id must be a positive integer!');
+  } else if (notes[id] !== undefined) {
+    res.status(200).send(notes[id]);
+  } else {
+    res.status(404).send('error: cannot find note with id: ' + id);
+  }
+});
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('listening on the port 3000');

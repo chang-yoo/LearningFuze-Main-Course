5c56d7c2bf2c92601c700b7af95af8f229872f3a
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-13 21:35:40 +0000

    in progress

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,24 +1,33 @@
 var images = ['images/001.png', 'images/004.png', 'images/007.png', 'images/039.png', 'images/025.png'];
-var carousel = setInterval(changeImage(images), 3000);
-clearInterval(carousel);
-
-function changeImage(object) {
-  var $img = document.querySelector('.images');
-  for (var i = 0; i < images.length; i++) {
-    $img.setAttribute('src', object[i]);
-  }
-  return $img;
-}
-
-const $rightI = document.querySelector('.right-angle');
-// console.log($rightI);
+let imageIndex = 0;
 
 const $image = document.querySelector('.images');
-// console.log($image);
-$rightI.addEventListener('click', nextImage);
 
-function nextImage(event) {
-  for (let i = 0; i < images.length; i++) {
-    $image.setAttribute('src', images[i + 1]);
+const carousel = setInterval(function () {
+  imageIndex++;
+  $image.setAttribute('src', $image[imageIndex]);
+  if (imageIndex === images.length - 1) {
+    imageIndex = 0;
+  }
+}, 3000);
+
+clearInterval(carousel);
+
+const $rightArrow = document.querySelector('.right-angle');
+// console.log($rightArrow);
+
+$rightArrow.addEventListener('click', nextImage);
+
+function nextImage() {
+  imageIndex++;
+  $image.setAttribute('src', images[imageIndex]);
+  if (imageIndex === images.length) {
+    imageIndex = 0;
   }
 }
+
+const $leftArrow = document.querySelector('.left-angle');
+$leftArrow.addEventListener('click', previousImage => {
+  imageIndex--;
+  $image.setAttribute('src', images[imageIndex]);
+});

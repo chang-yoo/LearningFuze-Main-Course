645a58ba10d5af0cfab1cece8e1e1c30292d03e0
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-13 00:49:12 +0000

    revised to parameterized queries for inputting to avoid sql injection attacks!

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -36,15 +36,42 @@ app.post('/api/grades', (req, res) => {
     res.status(400).json({ error: 'name, course, and score cannot be left as empty AND score need to be an integer from 0 to 100' });
     return;
   }
+  const sql = 'INSERT INTO grades(name, course, score) VALUES ($1, $2, $3) RETURNING*';
+  const values = [names, courses, scores];
+  db
+    .query(sql, values)
+    .then(result => {
+      res.status(201).send(result.rows[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send({ error: 'An unexpected error occured and failed to pull data from database' });
+    });
+});
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const beforeId = req.params.gradeId;
+  const id = parseInt(beforeId);
+  const body = req.body;
+  if (!Number.isInteger(id) || id <= 0 || body.name === undefined || body.course === undefined || body.score === undefined) {
+    res.status(400).send({ error: 'An invalid gradeId or missing name, course, or socre' });
+  }
   const sql = `
-    insert into "grades" ("name", "course", "score")
-                  values ('${names}', '${courses}', '${scores}')
-                  returning*
+    update "grades"
+    set "name" = 'body.name',
+        "course" = 'body.course',
+        "score" = body.score
+  where "gradeId" = ${id}
   `;
   db
     .query(sql)
     .then(result => {
-      res.status(201).send(result.rows[0]);
+      const grade = result.row[0];
+      if (grade) {
+        res.status(200).send(grade);
+      } else {
+        res.status(404).send({ error: `Cannot find grade with "gradeId of" ${id}` });
+      }
     })
     .catch(err => {
       console.error(err);

9750150c8023b0e9192e1b5d92ed9d8a1cf1c3be
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-13 00:08:49 +0000

    post feature completed

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-// const fs = require('fs');
 
 const pg = require('pg');
 
@@ -24,6 +23,32 @@ app.get('/api/grades', (req, res) => {
     .catch(err => {
       console.error(err);
       res.status(500).send({ error: 'An unexpected error occured.' });
+    });
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  const names = req.body.name;
+  const courses = req.body.course;
+  const scores = req.body.score;
+  if (names === undefined || courses === undefined || scores === undefined || scores < 0 || scores > 100) {
+    res.status(400).json({ error: 'name, course, and score cannot be left as empty AND score need to be an integer from 0 to 100' });
+    return;
+  }
+  const sql = `
+    insert into "grades" ("name", "course", "score")
+                  values ('${names}', '${courses}', '${scores}')
+                  returning*
+  `;
+  db
+    .query(sql)
+    .then(result => {
+      res.status(201).send(result.rows[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send({ error: 'An unexpected error occured and failed to pull data from database' });
     });
 });
 

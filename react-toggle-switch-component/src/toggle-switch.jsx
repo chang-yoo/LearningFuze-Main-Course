ed453df303545934b6dff5ee1ed5d62257daae67
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-19 04:25:47 +0000

    removed message property from this object and stated in render()

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -4,25 +4,23 @@ export default class Switch extends React.Component {
   constructor(props) {
     super(props);
     this.state = { status: 'off' };
-    this.message = 'off';
     this.switchOn = this.switchOn.bind(this);
     this.switchOff = this.switchOff.bind(this);
   }
 
   switchOn() {
     this.setState({ status: 'on' });
-    this.message = 'on';
   }
 
   switchOff() {
     this.setState({ status: 'off' });
-    this.message = 'off';
   }
 
   render() {
-    if (this.state.status === 'on') {
-      return <div className="on"><button className="onbox" onClick={this.switchOff}>switch</button>{this.message}</div>;
+    const toggle = this.state.status;
+    if (toggle === 'on') {
+      return <div className="on"><button className="onbox" onClick={this.switchOff}>switch</button>On</div>;
     }
-    return <div className="off"><button className="offbox" onClick={this.switchOn}>switch</button>{this.message}</div>;
+    return <div className="off"><button className="offbox" onClick={this.switchOn}>switch</button>Off</div>;
   }
 }

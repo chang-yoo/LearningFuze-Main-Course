bee9f0c356b5b7a1174a395ced66ea6a87d19efb
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-19 00:22:32 +0000

    revised this.state property to count

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -3,28 +3,26 @@ import React from 'react';
 export default class HotButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isClicked: false };
-    this.count = 0;
+    this.state = { count: 0 };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
-    this.setState({ isClicked: true });
-    this.count++;
+    this.setState(this.count++);
   }
 
   render() {
-    if (this.state && this.count < 4) {
+    if (this.count < 4) {
       return <div> <button className= 'purple' onClick={this.handleClick}>Hot Button</button> {this.count} click</div>;
-    } else if (this.state && this.count < 7) {
+    } else if (this.count < 7) {
       return <div> <button className='orchid' onClick={this.handleClick}>Hot Button</button> {this.count} click</div>;
-    } else if (this.state && this.count < 10) {
+    } else if (this.count < 10) {
       return <div> <button className='red' onClick={this.handleClick}>Hot Button</button> {this.count} click</div>;
-    } else if (this.state && this.count < 13) {
+    } else if (this.count < 13) {
       return <div> <button className='orange' onClick={this.handleClick}>Hot Button</button> {this.count} click</div>;
-    } else if (this.state && this.count < 18) {
+    } else if (this.count < 18) {
       return <div> <button className='yellow' onClick={this.handleClick}>Hot Button</button> {this.count} click</div>;
-    } else if (this.state && this.count >= 18) {
+    } else if (this.count >= 18) {
       return <div> <button className='white' onClick={this.handleClick}>Hot Button</button> {this.count} click</div>;
     }
   }

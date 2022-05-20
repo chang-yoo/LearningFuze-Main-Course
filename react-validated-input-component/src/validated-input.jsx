13a8c69ab6535ac92806690fccacd797cc9648fa
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-20 00:02:05 +0000

    completed except extra feature

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -11,15 +11,32 @@ export default class Validated extends React.Component {
 
   handlePassword(event) {
     this.setState({ password: event.target.value });
-    // console.log(this.state.password);
   }
 
   render() {
-    // const value = this.state.password;
-    return <form>
-      Password <br/>
-      <input name="password" type="password" value={this.state.password} onChange={this.handlePassword}>
-      </input>
-    </form>;
+    const value = this.state.password;
+    if (value === '') {
+      return <form>
+        Password <br />
+        <input name="password" type="password" value={this.state.password} onChange={this.handlePassword}>
+        </input> <i className="fa-solid fa-x"></i> <br/>
+        <p>A password is required.</p>
+      </form>;
+    } else if (value.length < 9) {
+      return <form>
+        Password <br />
+        <input name="password" type="password" value={this.state.password} onChange={this.handlePassword}>
+        </input> <i className="fa-solid fa-x"></i> <br />
+        <p>Your password is too short.</p>
+      </form>;
+    } else {
+      return (
+        <form>
+        Password <br/>
+        <input name="password" type="password" value={this.state.password} onChange={this.handlePassword}>
+          </input> <i className="fa-solid fa-check"></i> <br/>
+      </form>
+      );
+    }
   }
 }

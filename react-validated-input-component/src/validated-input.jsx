fb884520197b437d8e1034c3af789c117f9fc08c
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-20 00:49:08 +0000

    digit completed

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -30,12 +30,22 @@ export default class Validated extends React.Component {
         <p>Your password is too short.</p>
       </form>;
     } else {
+      if (value.includes('0') || value.includes('1') || value.includes('2') || value.includes('3') || value.includes('4') || value.includes('5') || value.includes('6') || value.includes('7') || value.includes('8') || value.includes('9')) {
+        return (
+        <form>
+          Password <br />
+          <input name="password" type="password" value={this.state.password} onChange={this.handlePassword}>
+          </input> <i className="fa-solid fa-check"></i> <br />
+        </form>
+        );
+      }
       return (
         <form>
-        Password <br/>
-        <input name="password" type="password" value={this.state.password} onChange={this.handlePassword}>
-          </input> <i className="fa-solid fa-check"></i> <br/>
-      </form>
+          Password <br />
+          <input name="password" type="password" value={this.state.password} onChange={this.handlePassword}>
+          </input> <i className="fa-solid fa-x"></i> <br />
+          <p>Your password requires number</p>
+        </form>
       );
     }
   }

e04f9ffc84aebfc33e1512bb5613b2de50e9b2be
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-20 01:02:13 +0000

    completed special character

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -30,21 +30,25 @@ export default class Validated extends React.Component {
         <p>Your password is too short.</p>
       </form>;
     } else {
-      if (value.includes('0') || value.includes('1') || value.includes('2') || value.includes('3') || value.includes('4') || value.includes('5') || value.includes('6') || value.includes('7') || value.includes('8') || value.includes('9')) {
-        return (
+      if (value.includes('0') || value.includes('1') || value.includes('2') || value.includes('3') || value.includes('4') || value.includes('5') || value.includes('6') ||
+         value.includes('7') || value.includes('8') || value.includes('9')) {
+        if (value.includes('!') || value.includes('@') || value.includes('#') || value.includes('$') || value.includes('%') || value.includes('^') || value.includes('&') ||
+          value.includes('*') || value.includes('(') || value.includes(')')) {
+          return (
         <form>
           Password <br />
           <input name="password" type="password" value={this.state.password} onChange={this.handlePassword}>
           </input> <i className="fa-solid fa-check"></i> <br />
         </form>
-        );
+          );
+        }
       }
       return (
         <form>
           Password <br />
           <input name="password" type="password" value={this.state.password} onChange={this.handlePassword}>
           </input> <i className="fa-solid fa-x"></i> <br />
-          <p>Your password requires number</p>
+          <p>Your password requires number/special character(!, @, #, $, %, ^, &, *, (, or ))/capital letter</p>
         </form>
       );
     }

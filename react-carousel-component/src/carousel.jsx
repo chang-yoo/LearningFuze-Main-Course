359abfb501f2c1ca3fc73190a3472a2f6c253f84
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-27 21:29:45 +0000

    in progress

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -11,32 +11,44 @@ export default class Carousel extends React.Component {
     this.previousImage = this.previousImage.bind(this);
     this.nextImage = this.nextImage.bind(this);
     this.dotColor = this.dotColor.bind(this);
+    this.imageSwap = this.imageSwap.bind(this);
   }
 
   previousImage() {
-    const current = this.state.current;
+    // clearInterval(this.timerId)
+    const { current } = this.state;
     if (current > 0) {
       this.setState({ current: current - 1, image: images[current] });
     } if (current === 0) {
       this.setState({ current: images.length - 1, image: images[current] });
     }
     this.dotColor();
+    // this.timerId = setInterval(() => this.nextImage(), 3000);
   }
 
   nextImage() {
-    const current = this.state.current;
+    // clearInterval(this.timerId)
+    const { current } = this.state;
     if (current >= 0) {
       this.setState({ current: current + 1, image: images[current] });
     }
-    if (this.state.current === images.length) {
-      this.setState({ current: 0, image: images[0] });
+    if (this.state.current === images.length - 1) {
+      this.setState({ current: 0, image: images[images.length - 1] });
     }
     this.dotColor();
+    // this.timerId = setInterval(() => this.nextImage(), 3000);
   }
 
   // componentDidMount() {
-  //   this.timerId = setInterval(() => this.nextImage(), 1000);
+  //   this.timerId = setInterval(() => this.nextImage(), 3000);
   // }
+
+  imageSwap(event) {
+    // clearInterval(this.timerId)
+    const dotId = event.target.id;
+    this.setState({ current: dotId, image: images[dotId] });
+    // this.timerId = setInterval(() => this.nextImage(), 3000);
+  }
 
   dotColor() {
     const dots = document.querySelectorAll('.fa-circle');
@@ -59,11 +71,11 @@ export default class Carousel extends React.Component {
         <img src={this.state.image}></img>
       </div>
       <div className="dot-container">
-         <i id="0" className="fas fa-circle horz-margin"></i>
-         <i id="1" className="far fa-circle horz-margin"></i>
-         <i id="2" className="far fa-circle horz-margin"></i>
-         <i id="3" className="far fa-circle horz-margin"></i>
-         <i id="4" className="far fa-circle horz-margin"></i>
+         <i id="0" onClick={this.imageSwap} className="fas fa-circle horz-margin"></i>
+         <i id="1" onClick={this.imageSwap} className="far fa-circle horz-margin"></i>
+         <i id="2" onClick={this.imageSwap} className="far fa-circle horz-margin"></i>
+         <i id="3" onClick={this.imageSwap} className="far fa-circle horz-margin"></i>
+         <i id="4" onClick={this.imageSwap} className="far fa-circle horz-margin"></i>
       </div>
     </div>;
   }

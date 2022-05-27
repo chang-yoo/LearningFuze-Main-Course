ef47dc8df465274f709021107b95b95b559fb046
Changhyun Yoo <chyoo0428@gmail.com>
2022-05-27 17:00:43 +0000

    in progress of removing lagging

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -10,52 +10,60 @@ export default class Carousel extends React.Component {
     };
     this.previousImage = this.previousImage.bind(this);
     this.nextImage = this.nextImage.bind(this);
+    this.dotColor = this.dotColor.bind(this);
   }
 
   previousImage() {
     const current = this.state.current;
-    clearInterval(this.intervalId);
     if (current > 0) {
       this.setState({ current: current - 1, image: images[current] });
-    } if (current <= 0) {
+    } if (current === 0) {
       this.setState({ current: images.length - 1, image: images[current] });
     }
-    this.intervalId = setInterval(() => this.nextImage(), 3000);
+    this.dotColor();
   }
 
-  // switchImage() {
-
-  // }
-
   nextImage() {
-    clearInterval(this.IntervalId);
     const current = this.state.current;
     if (current >= 0) {
       this.setState({ current: current + 1, image: images[current] });
     }
-    if (this.state.current >= images.length) {
+    if (this.state.current === images.length) {
       this.setState({ current: 0, image: images[0] });
     }
-    // this.intervalId = setInterval(() => this.nextImage(), 3000);
+    this.dotColor();
   }
 
-  componentDidMount() {
-    // this.intervalId = setInterval(() => this.nextImage(), 3000);
+  // componentDidMount() {
+  //   this.timerId = setInterval(() => this.nextImage(), 1000);
+  // }
+
+  dotColor() {
+    const dots = document.querySelectorAll('.fa-circle');
+    for (let i = 0; i < dots.length; i++) {
+      if (Number(dots[i].id) === this.state.current) {
+        dots[i].className = 'fas fa-circle horz-margin';
+      } else if (dots[i].id > 4) {
+        dots[0].className = 'fas fa-circle horz-margin';
+      } else {
+        dots[i].className = 'far fa-circle horz-margin';
+      }
+    }
   }
 
   render() {
     return <div className="container">
       <i onClick={this.previousImage} className="left-angle fa-solid fa-angle-left fa-2x"></i>
+       <i onClick={this.nextImage} className="right-angle fa-solid fa-angle-right fa-2x"></i>
       <div className="imagecontainer">
         <img src={this.state.image}></img>
       </div>
-      <i onClick={this.nextImage} className="right-angle fa-solid fa-angle-right fa-2x"></i>
       <div className="dot-container">
-        <i id="0" className="fas fa-circle horz-margin"></i>
-        <i id="1" className="far fa-circle horz-margin"></i>
-        <i id="2" className="far fa-circle horz-margin"></i>
-        <i id="3" className="far fa-circle horz-margin"></i>
-        <i id="4" className="far fa-circle horz-margin"></i>
+         <i id="0" className="fas fa-circle horz-margin"></i>
+         <i id="1" className="far fa-circle horz-margin"></i>
+         <i id="2" className="far fa-circle horz-margin"></i>
+         <i id="3" className="far fa-circle horz-margin"></i>
+         <i id="4" className="far fa-circle horz-margin"></i>
       </div>
     </div>;
   }
